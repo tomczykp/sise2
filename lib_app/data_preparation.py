c3684249f08bc6ab28cc65039fc1e44b1a31a3bf
tomczykp <tomczykp987@gmail.com>
2022-06-12 19:26:10 +0200

    Wersja z klasyfikacją

--- lib_app/data_preparation.py
+++ lib_app/data_preparation.py
@@ -5,7 +5,7 @@ from json import load, dump
 from os import path
 
 ILOSC_PKT = 225
-filename = "dane.json"
+filename = "dane"
 
 
 def loadTrainData(sala_nr):
@@ -51,15 +51,16 @@ def wyznacz_klasy(data):
 
 
 def prepare_data():
-	if path.isfile(f"x_{filename}"):
-		with open(f"x_{filename}", 'r') as file_object:
+	nr_sali = 10
+	if path.isfile(f"x_{filename}{nr_sali}.json"):
+		with open(f"x_{filename}{nr_sali}.json", 'r') as file_object:
 			x_train = load(file_object)
-		with open(f"y_{filename}", 'r') as file_object:
+		with open(f"y_{filename}{nr_sali}.json", 'r') as file_object:
 			y_train = load(file_object)
 		return x_train, y_train
 
 	print("DATA NOT IMPORTED .... LOADING FROM ORIGINAL FILES")
-	data = loadTrainData(8)
+	data = loadTrainData(10)
 	shuffle(data)
 	wyznacz_klasy(data)
 	x_train, y_train = [], []
@@ -69,12 +70,13 @@ def prepare_data():
 		x_train.append([xs[0], xs[1]])
 		y_train.append(target(xs[2], xs[3]))
 
-	with open(f"x_{filename}", 'w') as file_object:
+	with open(f"x_{filename}{nr_sali}.json", 'w') as file_object:
 		dump(x_train, file_object)
-	with open(f"y_{filename}", 'w') as file_object:
+	with open(f"y_{filename}{nr_sali}.json", 'w') as file_object:
 		dump(y_train, file_object)
 	return x_train, y_train
 
 
 def target(x, y):
 	return klasy[f"{int(x)}x{int(y)}"]
+
--- lib_app/main.py
+++ lib_app/main.py
@@ -16,18 +16,18 @@ def main():
 		assert not np.any(np.isnan(x))
 
 	model = tf.keras.models.Sequential([
-		tf.keras.layers.Dense(220, activation='selu'),
-		tf.keras.layers.Dense(140, activation='selu'),
-		tf.keras.layers.Dense(460, activation='selu'),
+		tf.keras.layers.Dense(120, activation='selu'),
+		tf.keras.layers.Dense(80, activation='selu'),
+		tf.keras.layers.Dense(260, activation='selu'),
 		tf.keras.layers.Dense(ILOSC_PKT)
 	])
 
 	loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-	opt = tf.keras.optimizers.Nadam(learning_rate=0.0012)
+	opt = tf.keras.optimizers.Nadam(learning_rate=0.0011)
 
 	model.compile(optimizer=opt, loss=loss_fn, metrics=['accuracy'])
 
-	model.fit(x_train, y_train, epochs=40)
+	model.fit(x_train, y_train, epochs=100)
 	model.evaluate(x_test, y_test, verbose=2)
 
 
